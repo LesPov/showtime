871a13c0ad3a8f540ef4a8144b6e821b2432071c
LesPo <139312751+LeonardoPove@users.noreply.github.com>
2024-07-10 08:07:13 -0500

    modific

--- src/app/three/components/cube.ts
+++ src/app/three/components/cube.ts
@@ -9,10 +9,5 @@ export function addCubes(scene: THREE.Scene) {
     cube1.position.set(0, 0, 0);
     scene.add(cube1);
 
-    // Añade el segundo cubo con dimensiones diferentes
-    const geometry2 = new THREE.BoxGeometry(5, 5, 2);
-    const material2 = new THREE.MeshBasicMaterial({ color: 0xffffff });
-    const cube2 = new THREE.Mesh(geometry2, material2);
-    cube2.position.set(3.5, 2.5, 0);
-    scene.add(cube2);
+   
 }
